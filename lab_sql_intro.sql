21a441011000fd22ffe629ca371ba2fd29e79da5
RetCast <danielretcast@hotmail.com>
2023-04-22 19:34:01 -0600

    [ADD] - Get film titles

--- lab_sql_intro.sql
+++ lab_sql_intro.sql
@@ -1,9 +1,22 @@
 USE sakila;
-SELECT * FROM actor;
-SELECT * FROM film;
-SELECT * FROM customer;
+#Get all the data from tables actor, film and customer.
+SELECT * FROM actor, film, customer;
+
+#Get film titles.
+SELECT title FROM film;
+
+#Get unique list of film languages under the alias language. 
+#Note that we are not asking you to obtain the language 
+#per each film, but this is a good time to think about how 
+#you might get that information in the future.
 SELECT DISTINCT `name` AS `language`
 FROM `language`;
+
+#Find out how many stores does the company have?
 SELECT COUNT(store_id) FROM store;
+
+#Find out how many employees staff does the company have?
 SELECT COUNT(manager_staff_id) FROM store;
+
+#Return a list of employee first names only?
 SELECT first_name FROM staff;
